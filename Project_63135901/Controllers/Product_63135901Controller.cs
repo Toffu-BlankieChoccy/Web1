a479f56da7b9433908a7918d99925cfb9ed877a8
Toffu-kum <trnhoangtrong@gmail.com>
2023-12-15 00:22:10 +0700

    P11 Done (vẫn lỗi tìm kiếm AJAX, trang cuối cùng, danh mục ở Home)

--- Project_63135901/Controllers/Product_63135901Controller.cs
+++ Project_63135901/Controllers/Product_63135901Controller.cs
@@ -37,16 +37,16 @@ namespace Project_63135901.Controllers
 
         }
 
-        [Route("/{Alias}--{id}", Name = "ListProduct")]
-        public IActionResult List(int id, int page = 1)
+        [Route("/{Alias}", Name = "ListProduct")]
+        public IActionResult List(string Alias, int page = 1)
 		{
             try
             {
                 var pageSize = 10; //Show 10 rows every page
-                var danhmuc = _context.Categories.Find(id);
+                var danhmuc = _context.Categories.AsNoTracking().SingleOrDefault(x => x.Alias == Alias);
                 var lsProducts = _context.Products
                    .AsNoTracking()
-                   .Where(x => x.CatId == id)
+                   .Where(x => x.CatId == danhmuc.CatId)
                    .OrderByDescending(x => x.DateCreated);
                 PagedList<Product> models = new PagedList<Product>(lsProducts, page, pageSize);
                 ViewBag.CurrentPage = page;
@@ -70,6 +70,16 @@ namespace Project_63135901.Controllers
                 {
                     return RedirectToAction("Index");
                 }
+
+                var lsProduct = _context.Products.AsNoTracking()
+                    .Where(x => x.CatId==product.CatId && x.ProductId!= id && x.Active == true)
+                    .OrderByDescending(x => x.DateCreated)
+                    .Take(4)
+                    .ToList();
+
+
+
+                ViewBag.SanPham = lsProduct;
                 return View(product);
             }
             catch
