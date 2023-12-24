ad8ade83d6a415d92a0046f9629ebb2cee72d2b2
Toffu-kum <trnhoangtrong@gmail.com>
2023-12-24 14:27:25 +0700

    P13 DONE

---
+++ Project_63135901/Controllers/Components/NumberCartViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_63135901.Extensions;
+using Project_63135901.ModelViews;
+
+namespace Project_63135901.Controllers.Components
+{
+    public class NumberCartViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var cart = HttpContext.Session.Get<List<CartItem>>("GioHang");
+            
+            return View(cart);
+        }
+    }
+}
